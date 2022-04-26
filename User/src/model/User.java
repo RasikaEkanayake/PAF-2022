2c97ea9afe090e3df33accbbf9e91ba57a68f757
Rasika Ekanayake <88251539+RasikaEkanayake@users.noreply.github.com>
2022-04-26 23:41:07 +0530

    update database name

--- User/src/model/User.java
+++ User/src/model/User.java
@@ -16,7 +16,7 @@ public class User
 	 Class.forName("com.mysql.jdbc.Driver"); 
 	 
 	 //Provide the correct details: DBServer/DBName, username, password 
-	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdb", "root", ""); 
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electricitypower", "root", ""); 
 	 } 
 	 catch (Exception e) 
 	 {e.printStackTrace();} 
